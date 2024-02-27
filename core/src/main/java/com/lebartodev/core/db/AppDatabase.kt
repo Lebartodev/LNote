982899def9029def59adfffd153b355927f5bb4d
lebarto <lebartodev@gmail.com>
2024-02-27 23:09:24 +0400

    Code improvements

--- core/src/main/java/com/lebartodev/core/db/AppDatabase.kt
+++ core/src/main/java/com/lebartodev/core/db/AppDatabase.kt
@@ -3,7 +3,6 @@ package com.lebartodev.core.db
 import androidx.room.Database
 import androidx.room.RoomDatabase
 import com.lebartodev.core.db.dao.NotesDAO
-import com.lebartodev.core.db.entity.Note
 import com.lebartodev.core.db.entity.NoteEntity
 import com.lebartodev.core.db.entity.Photo
 import com.lebartodev.core.di.utils.AppScope
